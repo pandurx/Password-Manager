d703d3336eadbd95d7e99858a1001fd372362190
pandurx <internalfire90@gmail.com>
2022-09-05 22:16:02 -0400

    add routes and other configs

--- src/App.js
+++ src/App.js
@@ -1,67 +1,17 @@
 //https://mui.com/material-ui/getting-started/usage/
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import Button from '@mui/material/Button';
+//https://stackblitz.com/edit/github-agqlf5?file=src%2Fmain.jsx
+import * as React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
-export default class App extends React.Component {
-  state = {
-    total: null,
-    next: null,
-    operation: null,
-  };
-  
-  handleClick = buttonName => {
-    console.log('handle click!')
-  };
-
-  render() {
-    return (
-      <div className="App">
-
-        <header className="App-header">
-   
-          <Button variant="contained">Hello World</Button>
-      
-
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
- 
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav style={{ borderBottom: 'solid 1px', paddingBottom: '1rem' }}>
+        <Link to="/invoices">Invoices</Link> |{' '}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
